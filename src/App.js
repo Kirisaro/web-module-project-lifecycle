643544bbb8b4d09c05dcbd54a683fbbb10eca270
Carlos Solano <solanoandy@gmail.com>
2022-01-11 14:18:31 -0600

    solved mvp, search bar, user, and app are done , had some trouble setting it up, confused on the es lint error i get from the = sign not sure how to fix that

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import axios from 'axios';
+import FollowerList from './components/FollowerList';
+import User from './components/User';
 
 class App extends React.Component {
-  render() {
-    return(<div>
-      Github Card
-    </div>);
-  }
+ state = {
+   user: {}
+ }
+ componentDidMount() {
+   axios.get('https://api.github.com/users/Kirisaro')
+   .then(response => {
+     this.setState({
+       ...this.setState,
+       user: response.data
+     })
+   })
+   .catch(errors => {
+     console.log(errors);
+   })
+ }
+
+ render() {
+   return (
+     <div>
+       <h2>My Github User Information</h2>
+       <User />
+       <FollowerList />
+     </div>
+   )
+ }
 }
 
 export default App;
